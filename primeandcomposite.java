7621c31de64fea2376e825627b74f9d7982dab26
saikirangurram811 <113358686+saikirangurram811@users.noreply.github.com>
2023-01-27 15:58:18 +0530

    Create primeandcomposite.java

---
+++ primeandcomposite.java
@@ -0,0 +1,33 @@
+//count all prime and composite numbers
+import java.util.*;
+class primeandcomposite
+{
+ public static void main(String[] args){
+ Scanner a=new Scanner(System.in);
+System.out.println("enter the size of array:");
+ int n=a.nextInt();
+ int[] arr=new int[n];
+ for(int i=0;i<n;i++){
+ arr[i]=a.nextInt();
+ }
+ int c1=0,prime=0,com=0;
+ for(int i=0;i<n;i++){
+ c1=0;
+ for(int j=1;j<=arr[i];j++){
+ 
+ if(arr[i]%j==0){
+ 
+ c1++;
+ }
+ }
+ 
+ if(c1==2){
+ prime++;
+ }
+ else if(c1>2){
+ com++;
+ }
+ }
+ System.out.println("prime="+prime+"\n"+"composite="+com);
+ }
+}
